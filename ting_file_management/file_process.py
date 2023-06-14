a1e387ce0a195c429cea29c67e8cb6d7e14ecbe7
Beatriz <beatriz.mediatto2@gmail.com>
2023-06-14 16:51:52 -0300

    feat: implementa função remove

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -19,7 +19,12 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if instance.__len__() < 0:
+        print("Não há elementos")
+        return None
+
+    delete = instance.dequeue()['nome_do_arquivo']
+    print(f"Arquivo {delete} removido com sucesso")
 
 
 def file_metadata(instance, position):
