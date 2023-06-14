67121fb580def8dbbf60e8d83f81e38f5b252176
Beatriz <beatriz.mediatto2@gmail.com>
2023-06-14 15:36:24 -0300

    feat: implementa função process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    lines = txt_importer(path_file)
+
+    for i in range(len(instance)):
+        if instance.search(i)['nome_do_arquivo'] == path_file:
+            return None
+
+    file = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines
+    }
+
+    instance.enqueue(file)
+    print(file)
 
 
 def remove(instance):
