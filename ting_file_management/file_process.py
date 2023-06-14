c7ac514fbd399f2d39251a3f8e2529361f7bc59a
Beatriz <beatriz.mediatto2@gmail.com>
2023-06-14 17:08:41 -0300

    feat: implementa função file metadata

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,4 +1,5 @@
 from ting_file_management.file_management import txt_importer
+import sys
 
 
 def process(path_file, instance):
@@ -28,4 +29,9 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if position > instance.__len__():
+        print('Posição inválida', file=sys.stderr)
+        return None
+
+    value = instance.search(position)
+    print(value)
